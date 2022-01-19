3efb24a5c247dfb7886876dd2a4e2cf8e1c71688
Domagoj Veštić <domagoj@MacBook-Pro-DV.local>
2022-01-19 19:27:15 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:globo_fitness/screens/bmi_screen.dart';
+import 'package:globo_fitness/screens/intro_screen.dart';
+
+void main() {
+  runApp(const GlobeApp());
+}
+
+class GlobeApp extends StatelessWidget {
+  const GlobeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.grey),
+      routes: {
+        '/': (context) => IntroScreen(),
+        '/bmi': (context) => BmiScreen(),
+      },
+      initialRoute: '/',
+    );
+  }
+}
---
+++ lib/screens/intro_screen.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import '../shared/menu_drawer.dart';
+import '../shared/menu_bottom.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("DV Fitness")),
+      drawer: MenuDrawer(),
+      bottomNavigationBar: MenuBottom(),
+      body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/gym.jpg'), fit: BoxFit.cover)),
+          child: Center(
+              child: Container(
+                  padding: EdgeInsets.all(24),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                    color: Colors.white70,
+                  ),
+                  child: Text("Per aspera ad astra",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 22, shadows: [
+                        Shadow(
+                            offset: Offset(1.0, 1.0),
+                            blurRadius: 2.0,
+                            color: Colors.grey)
+                      ]))))),
+    );
+  }
+}
